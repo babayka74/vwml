177b0be21bd064a47a7c7efd623f75836b33f65f
ogibayev <ogibayev@gmail.com>
2015-02-19 13:44:11 +0300

    ---

--- model/src/com/vw/lang/grammar/VirtualWorldModelingLanguageLexer.java
+++ model/src/com/vw/lang/grammar/VirtualWorldModelingLanguageLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 C:\\Users\\Oleg\\projects\\vwml\\model\\src\\com\\vw\\lang\\grammar\\VirtualWorldModelingLanguage.g 2015-02-13 01:08:23
+// $ANTLR 3.4 C:\\Users\\Oleg\\projects\\vwml\\model\\src\\com\\vw\\lang\\grammar\\VirtualWorldModelingLanguage.g 2015-02-17 23:30:23
  
 package com.vw.lang.grammar;
 
