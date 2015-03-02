98ed192352c68cbb46843ddfbf34e95f2dabd5fc
ogibayev <ogibayev@gmail.com>
2015-03-02 19:49:16 +0300

    msg: hiring_&_dismissing ready

--- model/src/com/vw/lang/grammar/VirtualWorldModelingLanguageParser.java
+++ model/src/com/vw/lang/grammar/VirtualWorldModelingLanguageParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 C:\\Users\\Oleg\\projects\\vwml\\model\\src\\com\\vw\\lang\\grammar\\VirtualWorldModelingLanguage.g 2015-02-22 23:03:15
+// $ANTLR 3.4 C:\\Users\\Oleg\\projects\\vwml\\model\\src\\com\\vw\\lang\\grammar\\VirtualWorldModelingLanguage.g 2015-03-02 12:21:00
 
 package com.vw.lang.grammar;
 
