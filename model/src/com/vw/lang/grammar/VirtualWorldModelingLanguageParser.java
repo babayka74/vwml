7b486330e115055e7998a42dc09e306f7d6a851b
ogibayev <ogibayev@gmail.com>
2015-03-23 23:41:42 +0300

    msg - implementing garage's functionality

--- model/src/com/vw/lang/grammar/VirtualWorldModelingLanguageParser.java
+++ model/src/com/vw/lang/grammar/VirtualWorldModelingLanguageParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.4 C:\\Users\\Oleg\\projects\\vwml\\model\\src\\com\\vw\\lang\\grammar\\VirtualWorldModelingLanguage.g 2015-03-20 12:41:13
+// $ANTLR 3.4 C:\\Users\\Oleg\\projects\\vwml\\model\\src\\com\\vw\\lang\\grammar\\VirtualWorldModelingLanguage.g 2015-03-23 13:22:49
 
 package com.vw.lang.grammar;
 
